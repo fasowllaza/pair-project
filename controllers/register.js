07ceecee113ec887f75f5675f7357d163c0f306b
diorvalend55 <w.valendrick@gmail.com>
2021-04-22 21:15:09 +0800

    ...

---
+++ controllers/register.js
@@ -0,0 +1,32 @@
+const { Customer, Bundle } = require('../models/')
+const bcrypt = require('bcryptjs');
+
+class Controller {
+    static getRegister (req, res) {
+        res.render('Register')
+    }
+
+    static postRegister (req, res) {
+        let salt = bcrypt.genSaltSync(10);
+        let hash = bcrypt.hashSync(req.body.password, salt)
+        
+        let input = {
+            name: (req.body.firstName + ' ' + req.body.lastName),
+            userName: req.body.userName,
+            address: req.body.address,
+            email: req.body.email,
+            noMobilePhone: req.body.noMobilePhone,
+            password: hash,
+        }
+
+        Customer.create(input)
+        .then(() => {
+            res.redirect('/login');
+        })
+        .catch((err) => {
+            res.send(err);
+        })
+    }
+}
+
+module.exports = Controller;
--- routes/login.js
+++ routes/login.js
@@ -1,1 +1,8 @@
+const express = require('express');
 const controller = require("../controllers/login")
+const loginRoutes = express.Router()
+
+
+loginRoutes.get('/', controller.login)
+
+module.exports = loginRoutes
