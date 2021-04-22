6c844c5f341d9b3b65b3b5c0c85277b53bd85a54
diorvalend55 <w.valendrick@gmail.com>
2021-04-22 16:47:12 +0800

    ..

--- models/bundle.js
+++ models/bundle.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Bundle.belongsToMany(models.Customer, { through: models.Transaction })
     }
   };
   Bundle.init({
--- models/customer.js
+++ models/customer.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Customer.belongsToMany(models.Bundle, { through: models.Transaction })
     }
   };
   Customer.init({
